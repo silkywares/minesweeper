bb7f136006aef727fe0386a7b761f8b94d90c80b
silkywares <143457068+silkywares@users.noreply.github.com>
2025-11-02 21:58:16 -0600

    labeled vertices

--- game.py
+++ game.py
@@ -1,33 +1,55 @@
 import pygame
+from pygame.math import Vector2
+import numpy
+import random
 
 SCREEN_DIM = 512
 CELL_DIM = 32
 
 def draw_grid():
     line_count = SCREEN_DIM / CELL_DIM
-    i = 0
+
     for i in range(int(line_count)):
         pygame.draw.line(screen, "black", ((i*CELL_DIM)+CELL_DIM,0), ((i*CELL_DIM)+CELL_DIM,SCREEN_DIM), 1)
         pygame.draw.line(screen, "black", (0,(i*CELL_DIM)+CELL_DIM), (SCREEN_DIM,(i*CELL_DIM)+CELL_DIM), 1)
-        
+
+def grid_vector_randomizer():
+    size = int(SCREEN_DIM / CELL_DIM)
+    grid = []
+    for y in range(size):
+        row = []
+        for x in range(size):
+            angle = random.uniform(0,360)
+            #vector = Vector2(1,0).rotate(angle)
+            row.append(angle)
+        grid.append(row)
+    return grid
+
+def perlin_noise(grid):
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            angle = grid[y][x]                  # get the stored angle
+            text_surface = font.render(f"{int(angle)}", True, (0, 0, 0))
+            screen.blit(text_surface, (x*CELL_DIM-9, y*CELL_DIM-9))
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_DIM,SCREEN_DIM))
 clock = pygame.time.Clock()
 running = True 
+font = pygame.font.Font(None, 18)
 
-font = pygame.font.Font(None, 36)
+grid_numbers = grid_vector_randomizer()
+
+screen.fill("tan")
 
 while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
     screen.fill("tan")
     draw_grid()
-    text_surface = font.render("Hello Pygame!", True, (0, 0, 0))
-    screen.blit(text_surface, (50, 50))
+    perlin_noise(grid_numbers)
     pygame.display.flip()
     clock.tick(60)
 
